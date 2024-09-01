6d5242ae2897e631e529c484461aae04bf7486b1
Vineeth K <vineeth@leuwint.in>
2024-09-01 14:51:34 +0530

    content for the portfolio added and fonts and navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import Navbar from "./components/Navbar";
 // import './App.css'
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
 
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!! </h1>
+      <Navbar/>
+      {/* <h1 className="text-3xl font-bold underline">Hello world!! </h1> */}
     </div>
   );
 }
---
+++ src/assets/content.js
@@ -0,0 +1,88 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const MAIN_CONTENT =
+  "I am a dedicated and passionate Software Engineer with over three years of experience, focusing on both frontend and backend development. I have a strong background in creating dynamic and user-centric web applications using React, Node.js, and RxJS. With a deep interest in technology and a commitment to continuous learning, I strive to deliver high-quality solutions across various domains, including healthcare, finance, and logistics.";
+export const ABOUT_TEXT =
+  "I am a Full Stack Developer with over three years of experience, primarily specializing in React and Node.js. My expertise lies in building responsive, user-friendly web applications, utilizing React hooks and RxJS for reactive programming. I have been involved in several projects across different domains, such as healthcare, finance, and logistics, leading both frontend and backend development. I thrive in collaborative environments and am passionate about optimizing performance and creating seamless user experiences. My recent work has focused on building applications that facilitate real-time data processing and efficient state management.";
+export const EXPERIENCE = [
+  {
+    year: "2023 - Present",
+    role: "Software Engineer",
+    company: "Leuwint Technologies",
+    description:
+      "Leading both frontend and backend development for a medical application supporting doctor-patient interactions and appointment management. Developing a doctor-centric React web interface for managing appointments, patient records, and online consultations. Regularly assisting with the Flutter and React teams.",
+    location: "Palakkad, Kerala",
+    technologies: [
+      "React",
+      "TypeScript",
+      "Ionic",
+      "Flutter",
+      "HTML",
+      "CSS",
+      "Node.js",
+      "RxJs",
+      "Git",
+    ],
+  },
+  {
+    year: "2020 - 2022",
+    role: "Developer / Trainer / Instructor",
+    company: "Technology Business Incubator (TBI), Jyothi Engineering College",
+    description:
+      "Collaborated in frontend development using React for various web projects. Provided training in HTML, CSS, JavaScript, and React to students. Designed and developed responsive frontend components enhancing user experience and compatibility across browsers.",
+    location: "Thrissur, Kerala",
+    technologies: ["React", "HTML", "CSS", "JavaScript"],
+  },
+];
+export const PROJECT = [
+  {
+    title: "Patient-Doctor Medical Application",
+    image: "project1.jpg",
+    description:
+      "Leading frontend and backend development for a medical application focused on doctor-patient interaction, appointment management, and online consultations.",
+    technologies: ["React", "Node.js", "Express", "RxJS", "Flutter"],
+  },
+  {
+    title: "Logistics Application",
+    image: "project2.jpg",
+    description:
+      "Leading backend and frontend development for a logistics application focusing on data flow, shipment tracking, and integration with Google Maps and HERE Maps.",
+    technologies: [
+      "React",
+      "Node.js",
+      "Express",
+      "RxJS",
+      "Google Maps",
+      "HERE Maps",
+    ],
+  },
+  {
+    title: "Financial Web Application",
+    image: "project3.jpg",
+    description:
+      "Led the development of a financial application focusing on transaction management and balance calculations, utilizing real-time data processing and reporting.",
+    technologies: ["React", "Node.js", "RxJS", "Express"],
+  },
+];
+export const CONTACT = {
+  address: "Thrissur, Kerala",
+  phoneNo: "+91 9744484888",
+  email: "kvineethvini94@gmail.com",
+  linkedin: "www.linkedin.com/in/vineethkondayil",
+};
+export const EDUCATION = [
+  {
+    degree: "B.Tech in Electrical and Electronics Engineering",
+    institution:
+      "Sreepathy Institute of Management and Technology, Calicut University",
+    year: "2012 - 2016",
+  },
+  {
+    degree: "MBA in Project Management",
+    institution: "Bharathiar University",
+    year: "2021 - Ongoing",
+  },
+];
