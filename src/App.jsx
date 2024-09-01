27413b4457f2fa430907f70092036b02e539b27c
Vineeth K <vineeth@leuwint.in>
2024-09-01 18:19:57 +0530

    navbar and buttons with background setted

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
 import Navbar from "./components/Navbar";
-// import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+      <div className="flixed top-0 -z-10 h-full w-full"> </div>
+     <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
+     
+      <div className="container mx-auto px-8">
       <Navbar/>
-      {/* <h1 className="text-3xl font-bold underline">Hello world!! </h1> */}
+      </div>
+
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,20 @@
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+<nav className="mb-20 flex items-center justify-between py-6 px-4">
+    <div className="flex flex-shrink-0 items-center">
+        <h2 className="text-black font-semibold text-2xl">VK</h2>
+    </div>
+    <div className="m-8 flex gap-10 text-2xl text-black">
+        <FaLinkedin/>
+        <FaInstagram/>
+        <FaGithub/>
+    </div>
+</nav>  
+)
 }
 
 export default Navbar;
